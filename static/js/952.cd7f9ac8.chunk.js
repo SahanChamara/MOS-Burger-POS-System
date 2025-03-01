6acb0c19e8b68784950a7d514c9995bd48a2437a
Sahan Chamara <sahanchamara456@gmail.com>
2025-03-01 21:44:42 +0530

    Updates

---
+++ static/js/952.cd7f9ac8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmos_burger_pos_system=self.webpackChunkmos_burger_pos_system||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.cd7f9ac8.chunk.js.map
